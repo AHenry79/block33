95657b4b435baa812fdb60999a1fb201b373c090
AHenry79 <austinhenry7799@gmail.com>
2024-05-31 12:00:01 -0400

    Add employees db and basic department db

---
+++ employees.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const pg = require("pg");
+
+const client = new pg.Client("postgres://localhost/hrdirectory");
+client.connect();
+
+// get all employees
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await client.query(
+      `SELECT * FROM employees ORDER BY id ASC`
+    );
+    res.send(response.rows);
+  } catch (err) {
+    next(err);
+  }
+});
+// get employees by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const response = await client.query(
+      `SELECT * FROM employees WHERE id = $1`,
+      [req.params.id]
+    );
+    res.send(response.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+// post new employee
+router.post("/", async (req, res, next) => {
+  try {
+    const response = await client.query(
+      `INSERT INTO employees(name, department_id, created_at) VALUES($1, $2, now())`,
+      [req.body.name, req.body.department_id]
+    );
+    res.send({
+      name: req.body.name,
+      department_id: req.body.department_id,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+// delete a flavor
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const response = await client.query(`DELETE from employees WHERE id =$1`, [
+      Number(req.params.id),
+    ]);
+    res
+      .send({
+        id: req.params.id,
+      })
+      .sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// update object data
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const response = await client.query(
+      `UPDATE employees SET name=$1, department_id=$2, updated_at=now() WHERE id=$3 RETURNING *`,
+      [req.body.name, req.body.department_id, Number(req.params.id)]
+    );
+    res.send(response.rows[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const pg = require("pg");
 const express = require("express");
-const client = new pg.Client(
-  process.env.DATABASE_URL || "postgres://localhost/the_acme_notes_db"
-);
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/employees", require("./employees"));
+app.use("/api/departments", require("./departments"));
+
+app.listen(8080, () => {
+  console.log("App is running at port 8080");
+});
